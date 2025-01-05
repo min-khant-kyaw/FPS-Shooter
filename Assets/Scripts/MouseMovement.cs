b646402fa51f96cf5efad19eef8e443f0b74e1ef
min-khant-kyaw <min.kkyaw0101@gmail.com>
2025-01-05 23:52:31 +0800

    basic movement

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 500f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+
+    void Start()
+    {
+        // Lock cursor in the middle of screen and make it invisible
+        Cursor.lockState = CursorLockMode.Locked;        
+    }
+
+
+    void Update()
+    {
+        // Getting the mouse inputs
+        // Up and Down
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        // Left and Right
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Rotation around the x-axis (Look up and down)
+        // Mouse mouse up, body rotates down and vice versa
+        xRotation -= mouseY;
+
+        // Clamp the rotation (sets limit so that funky things wont happen)
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        // Rotation around the y-axis (Look left and right)
+        yRotation += mouseX;
+
+        // Apply rotations to our transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
