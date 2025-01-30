3fa01a3ac4070317abadd8c319dcc08479380a12
min-khant-kyaw <min.kkyaw0101@gmail.com>
2025-01-31 02:06:31 +0800

    weapon clip fix + added HUD for weapons

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public static HUDManager Instance { get; set; }
+
+    [Header("Ammo")]
+    public TextMeshProUGUI magazineAmmoUI;
+    public TextMeshProUGUI totalAmmoUI;
+    public Image ammoTypeUI;
+
+    [Header("Weapon")]
+    public Image activeWeaponUI;
+    public Image inactiveWeaponUI;
+
+    [Header("Throwables")]
+    public Image lethalUI;
+    public TextMeshProUGUI lethalAmountUI;
+
+    public Image tacticalUI;
+    public TextMeshProUGUI tacticalAmountUI;
+
+    public Sprite emptySlot;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        Weapon activeWeapon = WeaponManager.Instance.activeWeaponSlot.GetComponentInChildren<Weapon>();
+        Weapon inactiveWeapon = GetInactiveWeaponSlot().GetComponentInChildren<Weapon>();
+
+        if (activeWeapon) {
+            magazineAmmoUI.text = $"{activeWeapon.bulletsLeft/activeWeapon.bulletsPerBurst}";
+            totalAmmoUI.text = $"{activeWeapon.magazineSize/activeWeapon.bulletsPerBurst}";
+
+            Weapon.WeaponModel model = activeWeapon.thisWeaponModel;
+            ammoTypeUI.sprite = GetAmmoSprite(model);
+            activeWeaponUI.sprite = GetWeaponSprite(model);
+
+            if (inactiveWeapon) {
+                inactiveWeaponUI.sprite = GetWeaponSprite(inactiveWeapon.thisWeaponModel);
+            }
+        } else {
+            magazineAmmoUI.text = "";
+            totalAmmoUI.text = "";
+        
+            ammoTypeUI.sprite = emptySlot;
+            activeWeaponUI.sprite = emptySlot;
+            inactiveWeaponUI.sprite = emptySlot;
+        }
+    }
+
+    private Sprite GetWeaponSprite(Weapon.WeaponModel model)
+    {
+        switch (model) {
+            case Weapon.WeaponModel.M1911:
+                return Resources.Load<GameObject>("M1911_Weapon").GetComponent<SpriteRenderer>().sprite;
+            case Weapon.WeaponModel.M4:
+                return Resources.Load<GameObject>("M4_Weapon").GetComponent<SpriteRenderer>().sprite;
+            default:
+                return null;
+        }
+    }
+    
+    private Sprite GetAmmoSprite(Weapon.WeaponModel model)
+    {
+        switch (model) {
+            case Weapon.WeaponModel.M1911:
+                return Resources.Load<GameObject>("Pistol_Ammo").GetComponent<SpriteRenderer>().sprite;
+            case Weapon.WeaponModel.M4:
+                return Resources.Load<GameObject>("Rifle_Ammo").GetComponent<SpriteRenderer>().sprite;
+            default:
+                return null;
+        }
+    }
+    
+    private GameObject GetInactiveWeaponSlot()
+    {
+        foreach (GameObject weaponSlot in WeaponManager.Instance.weaponSlots) {
+            if (weaponSlot != WeaponManager.Instance.activeWeaponSlot) {
+                return weaponSlot;
+            }
+        }
+        return null;
+    }
+
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -98,10 +98,6 @@ public class Weapon : MonoBehaviour
                 burstBulletsLeft = bulletsPerBurst;
                 FireWeapon();
             }
-
-            if (AmmoManager.Instance.ammoDisplay != null) {
-                AmmoManager.Instance.ammoDisplay.text = $"{bulletsLeft/bulletsPerBurst}/{magazineSize/bulletsPerBurst}";
-            }
         }
     }
 
